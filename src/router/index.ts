b6a89110e5e2680bd3861b33e23ef1a676c1cc08
Emil Valiev <evaliev90@gmail.com>
2024-07-02 12:48:25 +0500

    commit_htaccess

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,7 +4,9 @@ import { supabase } from '@/lib/supabaseClient'
 let userSession
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  // history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
+  // mode: 'history',
   routes: [
     {
       path: '/',
